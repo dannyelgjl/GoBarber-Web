1900b96c9bb38f94595d389345df3800f9b3b355
dannyelgjl <dannyelgjl@gmail.com>
2020-10-29 22:45:01 -0300

    feature/SingUp page finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import SingIn from './pages/SingIn';
+//import SingIn from './pages/SingIn';
+import SingUp from './pages/SingUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SingIn />
+      <SingUp />
       <GlobalStyle />
     </>
   );

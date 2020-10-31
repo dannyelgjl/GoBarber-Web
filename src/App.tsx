b8a09b15b19d22d43bb9e235de93ed829070b39c
dannyelgjl <dannyelgjl@gmail.com>
2020-10-31 18:30:13 -0300

    feature/Login validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-//import SingIn from './pages/SingIn';
-import SingUp from './pages/SingUp';
+import SingIn from './pages/SingIn';
+//import SingUp from './pages/SingUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SingUp />
+      <SingIn />
       <GlobalStyle />
     </>
   );
--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup'
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import logo from '../../assets/logo.svg';
@@ -9,12 +13,38 @@ import Button from '../../components/Button'
 import { Container, Content, Background } from './styles';
 
 const SingIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false, // = vai retorna todos os erros juntos
+      });
+
+      console.log(data)
+
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+      console.log(err);
+    }
+  }, []);
+  console.log(formRef);
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -22,7 +52,7 @@ const SingIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="#">
           <FiLogIn />

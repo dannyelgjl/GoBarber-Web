a5fadd6be9cb8018934b5ae81beffbc5d78f249b
dannyelgjl <dannyelgjl@gmail.com>
2020-11-09 10:39:12 -0300

    adding user data to local storage

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -23,7 +23,9 @@ interface SingInFormData {
 const SingIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { singIn } = useContext(AuthContext);
+  const { user, singIn } = useContext(AuthContext);
+
+  console.log(user);
 
   const handleSubmit = useCallback(async (data: SingInFormData) => {
     try {
